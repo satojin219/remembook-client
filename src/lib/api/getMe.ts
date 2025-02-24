463332e98581daec104904e7106c519f4b3344c2
satojin219 <renzuoteng142@gmail.com>
2025-02-24 17:27:23 +0900

    feat: getMeを実装

---
+++ src/lib/api/getMe.ts
@@ -0,0 +1,37 @@
+import type { APIResponse } from "@/types/common";
+import { type ErrorType, getErrorMessage } from "../error";
+import type { User } from "@/types/user";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export async function getBook(bookId: string): Promise<APIResponse<User>> {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken");
+  if (!accessToken) {
+    return redirect("/login");
+  }
+
+  try {
+    const res = await fetch(`${process.env.REMEMBOOK_API_URL}/api/v1/user/me`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken.value}`,
+        credentials: "include",
+      },
+    });
+    if (!res.ok) {
+      const errorResponse = (await res.json()) as ErrorType;
+      throw errorResponse;
+    }
+
+    const user = (await res.json()) as User;
+
+    return { ok: true, data: user };
+  } catch (e) {
+    return {
+      ok: false,
+      errorMessage: getErrorMessage((e as ErrorType).error.code),
+    };
+  }
+}
---
+++ src/types/user.ts
@@ -0,0 +1,5 @@
+export type User = {
+  userId: string;
+  coins: number;
+  loginedAt: string;
+};
