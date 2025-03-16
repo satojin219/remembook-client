a6597db40f6cd61738acd9945213a9330d9aa5bf
satojin219 <renzuoteng142@gmail.com>
2025-03-16 17:03:09 +0900

    feat:ファビコン、Headerのロゴを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,10 +12,10 @@ export const Header = async () => {
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <Link href="/" className="flex items-center">
           <Image
-            src="/logo.svg"
-            alt="logo"
-            width={120}
-            height={40}
+            src="/headerLogo.svg"
+            alt="remembook"
+            width={250}
+            height={60}
             className="object-contain"
           />
         </Link>
