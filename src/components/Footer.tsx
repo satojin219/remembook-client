8188bb66dbadcf5669af86c0fbd61b6dee1057da
satojin219 <renzuoteng142@gmail.com>
2025-03-16 16:09:38 +0900

    feat: Footerを追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import type { FC } from "react";
+
+export const Footer: FC = () => {
+  return (
+    <footer className=" bg-gray-50 border-t">
+      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-center space-x-6 text-sm text-gray-600">
+          <Link href="/terms" className="hover:text-gray-900 hover:underline">
+            利用規約
+          </Link>
+          <Link href="/privacy" className="hover:text-gray-900 hover:underline">
+            プライバシーポリシー
+          </Link>
+          <Link
+            href="/transaction"
+            className="hover:text-gray-900 hover:underline">
+            特定商取引法に基づく表示
+          </Link>
+          <Link
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfQAUV5nibf0Blw8Hq_tcp8HFU-lsUQOWYav2lA2e8csgpJMw/viewform?usp=dialog"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-900 hover:underline">
+            お問い合わせ
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
