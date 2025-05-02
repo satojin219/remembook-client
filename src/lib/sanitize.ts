d8bc6470c3460ff566884af9cd100c2c3bad9e4e
satojin219 <renzuoteng142@gmail.com>
2025-05-02 09:41:14 +0900

    chore: サニタイズのlibを追加

---
+++ src/lib/sanitize.ts
@@ -0,0 +1,9 @@
+export const sanitizeString = (str: string): string => {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#x27;")
+    .replace(/\//g, "&#x2F;");
+};
