2e37d624ff96a304857c39fd706ae7b335505139
satojin219 <renzuoteng142@gmail.com>
2025-10-22 22:42:49 +0900

    feat: GAを別スレッドに移行

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Footer } from "@/components/Footer";
 import Script from "next/script";
 import { headers } from "next/headers";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { Partytown } from "@qwik.dev/partytown/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,7 +52,21 @@ export default async function RootLayout({
         <main className="flex-1">{children}</main>
         <Footer />
         <Script src="/main-app.js" strategy="afterInteractive" nonce={nonce} />
-        <GoogleAnalytics gaId="G-N04844P4XD" />
+        <Partytown debug={true} forward={["dataLayer.push"]} />
+        <script
+          id="pt-gtag"
+          src="https://www.googletagmanager.com/gtag/js?id=G-N04844P4XD"
+          type="text/partytown"
+        />
+
+        <script id="pt-gtag-init" type="text/partytown">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-N04844P4XD', { send_page_view: false });
+          `}
+        </script>
       </body>
     </html>
   );
